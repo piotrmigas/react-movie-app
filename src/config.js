c6d2b437c0ea73590b964dd6c4fdfe1a7cec7650
piotrmigas <piotr.migas@icloud.com>
2021-06-17 22:25:44 +0200

    commit

--- src/config.js
+++ src/config.js
@@ -1,18 +1,15 @@
-// Configuration for TMDB
-// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
-// Read more about the API here: https://developers.themoviedb.org/
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
-//process.env.API_KEY;
+export const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
+export const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
 
-const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`;
-const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`;
+export const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
+export const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
+export const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
-const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-// Sizes: w300, w780, w1280, original
-const BACKDROP_SIZE = "w1280";
-// w92, w154, w185, w342, w500, w780, original
-const POSTER_SIZE = "w500";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
-export { SEARCH_BASE_URL, POPULAR_BASE_URL, API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+export const BACKDROP_SIZE = "w1280";
+
+export const POSTER_SIZE = "w780";
